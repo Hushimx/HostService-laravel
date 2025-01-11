d4797588651e23a2352e14e631a2d60e62daa40e
Mahmoud <yousry.mohmmed@rocketmail.com>
2025-01-11 20:37:08 +0200

    routes shortenend

--- routes/web.php
+++ routes/web.php
@@ -28,90 +28,60 @@ Route::group(
     'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
   ],
 function(){
-    // users Authentication
+    # users Authentication
     Auth::routes(['guard' => 'vendors']);
     Route::view('/user/password/forget', 'auth/passwords/email')->name('password.forgetpassword');
 
-    // Global Routes
+    # Global Routes
     Route::redirect('/', '/vendor/dashboard');
     Route::redirect('/home', '/vendor/dashboard')->name('home');
 
-});
+    # guest vendors Only
+    Route::middleware('guest:vendors')->group(function () {
+      // vendor Authentication
+      Route::get('vendor/login', [VendorLoginController::class, 'showLoginForm'])->name('vendor.login.form');
+      // vendor Authentication
+      Route::post('/vendor/login', [VendorLoginController::class, 'login'])->name('vendor.login');
+    });
 
-/*
-|--------------------------------------------------------------------------
-| guest vendors Only
-|--------------------------------------------------------------------------
-| these routes for guest of vendors can access only
-|
-|
-*/
+    # Vendors Routes Only Can Access
+    Route::middleware('vendor')->group(function () {
+      // vendor dashboard
+      Route::get('/vendor/dashboard', [vendorsDashboardController::class, 'index'])->name('vendor.dashboard');
 
-Route::group(
-  [
-    'prefix' => LaravelLocalization::setLocale(),
-    'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath', 'guest:vendors']
-  ],
-function(){
-  // vendor Authentication
-  Route::get('vendor/login', [VendorLoginController::class, 'showLoginForm'])->name('vendor.login.form');
-  // vendor Authentication
-  Route::post('/vendor/login', [VendorLoginController::class, 'login'])->name('vendor.login');
-});
+      Route::post('/vendor/logout', [VendorLoginController::class, 'logout'])->name('vendor.logout');
 
+      // deliveryOrders
+      Route::get('delivery-orders', [DeliveryOrdersController::class, 'index'])->name('deliveryOrders.index');
+      Route::get('delivery-orders/delivery-order-items/{id}', [DeliveryOrdersController::class, 'deliveryOrderItems'])->name('deliveryOrders.deliveryOrderItems');
 
-/*
-|--------------------------------------------------------------------------
-| Vendors Web Routes Only Can Access
-|--------------------------------------------------------------------------
-| these routes for vendors can acces only
-|
-|
-*/
+      // vendor services
+      Route::get('vendor-services', [VendorServicesController::class, 'index'])->name('services.index');
+      Route::get('vendor-services/{serviceId}', [VendorServicesController::class, 'edit'])->name('services.edit'); // to edit the price
+      Route::get('vendor-services/edit/{id}', [VendorServicesController::class, 'editDesc'])->name('service.edit.description');
+      Route::put('vendor-services/update/{id}', [VendorServicesController::class, 'updateDesc'])->name('service.update.description');
 
-Route::group(
-  [
-    'prefix' => LaravelLocalization::setLocale(),
-    'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath', 'vendor']
-  ],
-function(){
+      // service orders
+      Route::get('service-orders', [ServiceOrdersController::class, 'index'])->name('service.orders.index');
 
-    // vendor dashboard
-    Route::get('/vendor/dashboard', [vendorsDashboardController::class, 'index'])->name('vendor.dashboard');
+      // stores
+      Route::get('vendor-stores', [StoresController::class, 'index'])->name('stores.index');
+      Route::get('vendor-stores/edit/{storeId}', [StoresController::class, 'edit'])->name('stores.edit');
+      Route::put('vendor-stores/update/{store}', [StoresController::class, 'update'])->name('stores.update');
 
-    Route::post('/vendor/logout', [VendorLoginController::class, 'logout'])->name('vendor.logout');
+      // profile edit
+      Route::view('edit/profile', 'avendor.profile-edit')->name('profile.edit');
 
-    // deliveryOrders
-    Route::get('delivery-orders', [DeliveryOrdersController::class, 'index'])->name('deliveryOrders.index');
-    Route::get('delivery-orders/delivery-order-items/{id}', [DeliveryOrdersController::class, 'deliveryOrderItems'])->name('deliveryOrders.deliveryOrderItems');
+      // products by storeId only
+      Route::get('products/bystore/{id}', [ProductController::class, 'storeProducts'])->name('products.store.index');
 
-    // vendor services
-    Route::get('vendor-services', [VendorServicesController::class, 'index'])->name('services.index');
-    Route::get('vendor-services/{serviceId}', [VendorServicesController::class, 'edit'])->name('services.edit'); // to edit the price
-    Route::get('vendor-services/edit/{id}', [VendorServicesController::class, 'editDesc'])->name('service.edit.description');
-    Route::put('vendor-services/update/{id}', [VendorServicesController::class, 'updateDesc'])->name('service.update.description');
-
-    // service orders
-    Route::get('service-orders', [ServiceOrdersController::class, 'index'])->name('service.orders.index');
-
-    // stores
-    Route::get('vendor-stores', [StoresController::class, 'index'])->name('stores.index');
-    Route::get('vendor-stores/edit/{storeId}', [StoresController::class, 'edit'])->name('stores.edit');
-    Route::put('vendor-stores/update/{store}', [StoresController::class, 'update'])->name('stores.update');
-
-    // profile edit
-    Route::view('edit/profile', 'avendor.profile-edit')->name('profile.edit');
-
-    // products by storeId only
-    Route::get('products/bystore/{id}', [ProductController::class, 'storeProducts'])->name('products.store.index');
-
-    // products Controller
-    Route::resource('products', ProductController::class);
+      // products Controller
+      Route::resource('products', ProductController::class);
+    });
 
 });
 
-
-// Fallback route
+# Fallback route
 Route::fallback(function () {
   return redirect('/home'); // Redirect to the desired route
 });
